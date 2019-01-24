a4e87888a247addd90d7687d0d415df439d22c0f
Mixan Panama <mixanpanama@gmail.com>
2019-01-24 22:06:18 +0200

    Car display into table functionality was implemented

--- src/main/java/com/training/carfactory/model/service/impl/DefaultCarService.java
+++ src/main/java/com/training/carfactory/model/service/impl/DefaultCarService.java
@@ -1,8 +1,14 @@
 package com.training.carfactory.model.service.impl;
 
 import com.training.carfactory.model.dao.CarDao;
+import com.training.carfactory.model.entity.Body;
 import com.training.carfactory.model.entity.Car;
+import com.training.carfactory.model.entity.Engine;
+import com.training.carfactory.model.entity.Wheels;
 import com.training.carfactory.model.service.CarService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DefaultCarService implements CarService {
 
@@ -17,4 +23,21 @@ public class DefaultCarService implements CarService {
         carDao.addCar(car);
     }
 
+    @Override
+    public List<Car> getAll() {
+        List<Car> cars = new ArrayList<>();
+        Car car = new Car();
+        Body body = new Body();
+        Engine engine = new Engine();
+        Wheels wheels = new Wheels();
+        body.setName("Nick");
+        engine.setName("Alexander");
+        wheels.setName("Victor");
+        car.setId(1L);
+        car.setBody(body);
+        car.setEngine(engine);
+        car.setWheels(wheels);
+        cars.add(car);
+        return cars;
+    }
 }
